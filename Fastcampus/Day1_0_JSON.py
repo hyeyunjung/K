0dca868312a2bbaf3e2813e00956da683cb06ca1
hyeyunjung <hyeyunijung@gmail.com>
2017-04-21 09:06:30 +0900

    ddsdf

---
+++ Fastcampus/Day1_0_JSON.py
@@ -0,0 +1,22 @@
+
+representor = {
+    'name': '김현중',
+    'age': 30,
+    'interests': ['text mining',
+                  'natural language processing'],
+    'friends': {
+        'name': '고태훈',
+        'id': 'kth'
+    }
+}
+
+representor['name']
+
+representor['friends']['name']
+
+print(representor)
+
+from pprint import pprint
+
+pprint(representor)
+
---
+++ Fastcampus/Day1_1_KoNLPy.py
@@ -0,0 +1,14 @@
+from konlpy.tag import Kkma, Twitter, Hannanum
+from pprint import pprint
+
+taggers = [('kkma', Kkma()), ('twitter', Twitter()), ('hannanum', Hannanum())]
+
+for tagger in taggers:
+    name = tagger[0]       # tagger라는 tuple의 0번째 요소
+    tagger = tagger[1]     # tagger라는 tuple의 1번째 요소
+    print('tagger name = %10s\tclass_name = %s' % (name, tagger.__class__))
+
+'%.3f' % 1.3205533
+
+
+eeer
